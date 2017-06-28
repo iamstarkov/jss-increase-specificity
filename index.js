f950a2fee19abf24aa1be566b69fd7f61bdacfd4
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2017-06-28 13:37:55 +0200

    backport jss-increase-id-specificit

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const increaseSpecificity = (repeat=3) => rule => {
-  rule.selector = ':root'.repeat(repeat) + ' ' + rule.selector;
-};
+/**
+ * use :not(#\20), :not(.\20) and :not(\20) instead of generating unlikely
+ * appearing ids…
+ * — twitter.com/subzey/status/829050478721896448
+ * Rationale: \20 is a css escape for U+0020 Space, and neither classname,
+ * nor id, nor tagname can contain a space
+ * — twitter.com/subzey/status/829051085885153280
+ */
 
-module.exports = increaseSpecificity;
+const selector = ':not(#\\20)';
+
+module.exports = function increaseSpecificity({ repeat }={ repeat: 3 }) {
+  const prefix = Array(repeat + 1).join(selector);
+  const onProcessSheet = (sheet) => {
+    sheet.rules.index.forEach(rule => {
+      if (rule.type === 'conditional') {
+        return onProcessSheet(rule);
+      }
+
+      rule.selectorText = prefix + rule.selectorText;
+    });
+  };
+
+  return { onProcessSheet };
+}
